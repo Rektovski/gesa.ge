1592654aa02e4a8c9c678fb81166df5dc85e3d36
Rektovski <murmanishvili94@gmail.com>
2022-12-13 04:08:07 +0400

    Burger Menu added

--- src/main/components/NavigationBar.js
+++ src/main/components/NavigationBar.js
@@ -1,6 +1,6 @@
 import "../design/NavigationBarStyle.css";
 import {Tabs, TabList, TabPanels, TabPanel, Button, Box, useTab, useMultiStyleConfig} from '@chakra-ui/react';
-import React from "react";
+import React, {useState} from "react";
 import Home from "./Home";
 import MDClub from "./MDClub";
 import AboutUs from "./AboutUs";
@@ -9,9 +9,14 @@ import Contact from "./Contact";
 import {MdTouchApp as TouchIcon} from "react-icons/md";
 
 export default function NavigationBar() {
+    const [menu, setMenu] = useState(false);
+    const rect1 = {transform: "rotate(405deg)", position: "relative", top: 7, backgroundColor: "white"};
+    const rect2 = {transform: "rotate(1485deg)", backgroundColor: "white"};
+    const rect3 = {transform: "rotate(-405deg)", position: "relative", top: -7, backgroundColor: "white"};
+
     const CustomTab = React.forwardRef((props, ref) => {
         // 1. Reuse the `useTab` hook
-        const tabProps = useTab({ ...props, ref })
+        const tabProps = useTab({...props, ref})
         const isSelected = !!tabProps['aria-selected']
 
         // 2. Hook into the Tabs `size`, `variant`, props
@@ -20,7 +25,7 @@ export default function NavigationBar() {
         return (
             <Button __css={styles.tab} {...tabProps}>
                 <Box as='span' mr='2' className={'d-flex align-items-center'}>
-                    {isSelected ? <TouchIcon size={40}/> : <>{tabProps.children}</>}
+                    {isSelected ? <TouchIcon size={40}/> : <span className={'NavbarTags'}>{tabProps.children}</span>}
                 </Box>
             </Button>
         )
@@ -28,14 +33,40 @@ export default function NavigationBar() {
 
     return (
         <>
-            <Tabs>
-                <TabList>
-                    <CustomTab>მთავარი</CustomTab>
-                    <CustomTab>კლუბი</CustomTab>
-                    <CustomTab>აკადემია</CustomTab>
-                    <CustomTab>ვინ ვართ</CustomTab>
-                    <CustomTab>კონტაქტი</CustomTab>
-                </TabList>
+            <Tabs className={'Navbar'}>
+                {
+                    window.innerWidth > 750 ?
+                        <TabList>
+                            <CustomTab>მთავარი</CustomTab>
+                            <CustomTab>კლუბი</CustomTab>
+                            <CustomTab>აკადემია</CustomTab>
+                            <CustomTab>ვინ ვართ</CustomTab>
+                            <CustomTab>კონტაქტი</CustomTab>
+                        </TabList>
+
+                        :
+
+                        <>
+                            <div className={'NavbarMenuIcon'}>
+                                <div className={`NavbarBurger`} onClick={() => setMenu(!menu)}>
+                                    <div style={menu ? rect1 : {}} className={'rectangle1'}></div>
+                                    <div style={menu ? rect2 : {}} className={'rectangle2'}></div>
+                                    <div style={menu ? rect3 : {}} className={'rectangle3'}></div>
+                                </div>
+                                {
+                                    menu ?
+                                        <div className={'menuNavbarLinks'}>
+                                            <CustomTab className={'navbarLinks'}>მთავარი</CustomTab>
+                                            <CustomTab className={'navbarLinks'}>კლუბი</CustomTab>
+                                            <CustomTab className={'navbarLinks'}>აკადემია</CustomTab>
+                                            <CustomTab className={'navbarLinks'}>ვინ ვართ</CustomTab>
+                                            <CustomTab className={'navbarLinks'}>კონტაქტი</CustomTab>
+                                        </div>
+                                        : ""
+                                }
+                            </div>
+                        </>
+                }
                 <TabPanels>
                     <TabPanel>
                         <Home/>
